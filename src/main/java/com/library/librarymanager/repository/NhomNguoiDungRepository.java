23ef6d8440266bebcd6e429bec8f084cffc6564b
Dang Tran Thien <dangtranthien2006@gmail.com>
2026-05-31 18:57:14 +0700

    fixed bugs

--- src/main/java/com/library/librarymanager/repository/NhomNguoiDungRepository.java
+++ src/main/java/com/library/librarymanager/repository/NhomNguoiDungRepository.java
@@ -3,10 +3,13 @@ package com.library.librarymanager.repository;
 
 import com.library.librarymanager.entity.NhomNguoiDung;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface NhomNguoiDungRepository extends JpaRepository<NhomNguoiDung, Integer> {
-    Optional<NhomNguoiDung> findByTenNhom(String tenNhom);
+    @Query(value = "SELECT * FROM nhom_nguoi_dung WHERE ten_nhom = :tenNhom LIMIT 1", nativeQuery = true)
+    Optional<NhomNguoiDung> findByTenNhom(@Param("tenNhom") String tenNhom);
 
 }
